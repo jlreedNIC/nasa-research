d47993181de82f55b52c95ec5fa1f8a447b208e8
Jordan Reed <reed5204@vandals.uidaho.edu>
2024-06-21 15:30:06 -0700

    commented out triangle face implementation in shape as procrustes comparison performs better. added procrustes implementation

--- shape.py
+++ shape.py
@@ -9,6 +9,7 @@
 from triangles import Triangle
 from stl import mesh
 import numpy as np
+from procrustes import generic
 
 class Shape:
     def __init__(self, stl_file):
@@ -28,10 +29,11 @@ class Shape:
 
         model_mesh = mesh.Mesh.from_file(stl_file)
         
-        # set faces
-        print(f'-- Grabbing {model_mesh.data['vectors'].shape[0]} triangles...')
-        for vector in model_mesh.data['vectors']:
-            self.faces.append(Triangle(vector))
+        # triangles not needed with procrustes comparison
+        # # set faces
+        # print(f'-- Grabbing {model_mesh.data['vectors'].shape[0]} triangles...')
+        # for vector in model_mesh.data['vectors']:
+        #     self.faces.append(Triangle(vector))
         
         # print(f'vector shape: {model_mesh.data['vectors'].shape}')
         # print(f'faces shape: {len(self.faces)}')
@@ -46,11 +48,50 @@ class Shape:
         # remove duplicate values
         self.point_cloud = np.unique(self.point_cloud, axis=0)
 
-        # print(f'point cloud attr: {self.point_cloud.shape}')
+    def compare_with_procrustes(self, qcloud, scale=False, threshhold=.005, rounding=4):
+        """
+        Compares 2 point clouds using the Procrustes method. Then compares computed point clouds to see how close each model is. Also computes frobenius norm, and root mean squared error. Accuracy score is based off threshold passed in. Can include scaling or not (see procrustes for more explanation). 
+
+        :param qcloud: point cloud of second model, used as reference model
+        :param scale: whether or not to allow scaling, defaults to False
+        :param threshhold: determines how close points are to determine if they 'match' for accuracy score, defaults to .005
+        :param rounding: how many decimals to round to, defaults to 4
+        :return: frobenius norm, root mean squared error, 'accuracy' score
+        """
+        result = generic(self.point_cloud, qcloud, translate=True, scale=scale)
+        frob_error = np.round(result.error, 4)
+
+        # new reference matrix
+        new_b = np.round(result.new_b, rounding)
+
+        # transformed matrix p
+        transformed = np.dot(result.new_a, result.t)
+        transformed = np.round(transformed, rounding)
+
+        # get root mean squared error (standard deviation of all errors)
+        rmse = np.sqrt(np.mean(np.sum((transformed-result.new_b)**2, axis=1)))
+        rmse = np.round(rmse, rounding)
+
+        # give accuracy measure
+        # find error=0
+        matched = transformed == new_b
+        count_matched = np.count_nonzero(matched.all(1))
+
+        approx_match = np.round((transformed-result.new_b),rounding) <= threshhold
+        count_approx_match = np.count_nonzero(approx_match.all(1))
+
+        comparison_score = np.round(count_approx_match/transformed.shape[0],rounding)
+
+        # print(f'matched: {count_matched} approx match: {count_approx_match}')
+        # print(f'total: {transformed.shape[0]}')
+        # print(f'Frobenius score: {frob_error} RMSE: {rmse} Score: {comparison_score*100}%')
+
+        return frob_error, rmse, comparison_score
 
     def remove_dup_points_from_point_cloud(self, q_cloud):
         """
-        Remove the duplicate points from a point cloud you are comparing.
+        DEPRACATED WITH ADDITION OF PROCRUSTES
+        Remove the duplicate points from a point cloud you are comparing your point cloud to.
 
         :param q_cloud: point cloud of another shape
         :returns
@@ -81,6 +122,7 @@ class Shape:
         return model2_no_dup, model2_dup, total_verts
 
     def compare_shapes(self, other_shape, athresh=None):
+        # DEPRACATED WITH ADDITION OF PROCRUSTES
         if athresh is None:
             athresh=self.approx_thresh
         print('starting comparison...')
